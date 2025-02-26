1520b86ea5a508b1b1f43d9157a4fc4cd5dd17b4
Shrabani Dutta <shrabani.dutta1@ingka.ikea.com>
2025-02-26 22:34:06 +0100

    add sample file to test

---
+++ src/calculator.js
@@ -0,0 +1,9 @@
+function addNumbers(a, b) {
+    return a + b;
+}
+
+const num1 = 5;
+const num2 = 10;
+console.log(`The sum of ${num1} and ${num2} is:`, addNumbers(num1, num2));
+
+module.exports = addNumbers;
